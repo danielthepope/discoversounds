573a08b4e829053e7676fe3b1ae6942a41be3b51
Dan Pope <danielthepope@users.noreply.github.com>
2019-05-26 12:38:12 +0100

    Longer IDs

--- discoversounds/models.py
+++ discoversounds/models.py
@@ -7,10 +7,10 @@ from discoversounds.database import Base
 class Show(Base):
     __tablename__ = 'shows'
     __table_args__ = {'extend_existing': True}
-    vpid = Column(String(30), primary_key=True)
-    epid = Column(String(30))
-    ipid = Column(String(30))
-    sid = Column(String(30), nullable=False)
+    vpid = Column(String(40), primary_key=True)
+    epid = Column(String(40))
+    ipid = Column(String(40))
+    sid = Column(String(40), nullable=False)
     title = Column(String(255), nullable=False)
     synopsis = Column(String(1024))
     availability_from = Column(DateTime())
@@ -20,14 +20,14 @@ class Show(Base):
 class ShowToArtist(Base):
     __tablename__ = 'show_to_artist'
     __table_args__ = {'extend_existing': True}
-    vpid = Column(String(30), primary_key=True)
+    vpid = Column(String(40), primary_key=True)
     artist = Column(String(255), primary_key=True)
     expiry = Column(DateTime())
 
 class Service(Base):
     __tablename__ = 'services'
     __table_args__ = {'extend_existing': True}
-    sid = Column(String(30), primary_key=True)
-    name = Column(String(30))
+    sid = Column(String(40), primary_key=True)
+    name = Column(String(40))
     local = Column(Boolean())
 
