2ddd3bc550a9287bb9f36c3579aa129ff62d82b4
Dan Pope <danielthepope@users.noreply.github.com>
2022-03-19 23:34:26 +0000

    Lock dependencies

--- setup.py
+++ setup.py
@@ -6,11 +6,19 @@ setup(
     url='https://github.com/danielthepope/discoversounds',
     packages=find_packages(),
     install_requires=[
+        'aniso8601==9.0.1',
+        'click==7.1.2',
         'Flask==1.0.2',
         'Flask-RESTful==0.3.7',
+        'itsdangerous==1.1.0',
+        'Jinja2==2.11.3',
+        'MarkupSafe==1.1.1',
         'python-dotenv==0.10.1',
+        'pytz==2021.1',
+        'six==1.16.0',
         'SQLAlchemy==1.3.2',
-        'Unidecode==1.0.23'
+        'Unidecode==1.0.23',
+        'Werkzeug==1.0.1',
     ],
     extras_require={
         'dev': [
